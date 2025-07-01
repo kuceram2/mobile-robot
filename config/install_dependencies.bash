853aaef9394f3bf5ebbfdfa8bfb4633189de6572
Martin Kucera <skokankucera@seznam.cz>
2025-07-01 15:53:06 +0100

    added gazebo camera and depth camera

--- config/install_dependencies.bash
+++ config/install_dependencies.bash
@@ -5,3 +5,6 @@ sudo apt install ros-humble-slam-toolbox
 
 echo "installing Nav2"
 sudo apt install ros-humble-navigation2 ros-humble-nav2-bringup
+
+echo "installing image compression pkg"
+sudo apt install ros-humble-image-transport-plugins
