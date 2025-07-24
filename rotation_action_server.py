7c69f919d5ac52e1bc8f5df6155461942075f1c7
Martin Kucera <skokankucera@seznam.cz>
2025-07-24 10:31:40 +0000

    working rotation server

--- rotation_action_server.py
+++ rotation_action_server.py
@@ -1,56 +1,99 @@
 import rclpy
-from rclpy.action import ActionClient
+from rclpy.action import ActionServer
 from rclpy.node import Node
+from rclpy.executors import MultiThreadedExecutor
+
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist
+
+import time
 
 #from action_tutorials_interfaces.action import Fibonacci
-from tutorial_interfaces.action import Fibonacci
+from tutorial_interfaces.action import Rotate
 
-class FibonacciActionClient(Node):
+class RotationActionServer(Node):
 
     def __init__(self):
-        super().__init__('fibonacci_action_client')
-        self._action_client = ActionClient(self, Fibonacci, 'fibonacci')
+        super().__init__('rotate_action_server')
+        self._action_server = ActionServer(
+            self,
+            Rotate, #interface name
+            'rotate', # action name (both must match with action client)
+            self.execute_callback)
+        
+        # subscriber for getting current rotation
+        self.subscription = self.create_subscription(Vector3, 'orientation_euler', self.listener_callback, 10)
+        self.subscription  # prevent unused variable warning
+        self.current_angle = 0.0
+        self.accumulated_angle = 0.0
 
-    def send_goal(self, order):
-        goal_msg = Fibonacci.Goal()
-        goal_msg.order = order
+        # publisher for commanding the robot
+        self.publisher_ = self.create_publisher(Twist, 'diff_cont/cmd_vel_unstamped', 10)
 
-        self._action_client.wait_for_server()
+    # Update accumulated angle with wrap-around correction
+    def update_continuous_angle(self, current_angle, last_angle, accumulated_angle):
+        delta = current_angle - last_angle
+        if delta > 180:
+            delta -= 360
+        elif delta < -180:
+            delta += 360
+        accumulated_angle += delta
+        return accumulated_angle
+    
+    # Rotate relative: returns target and updated state
+    def rotate_relative(self, accumulated_angle, relative_rotation):
+        return accumulated_angle + relative_rotation
 
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+    def listener_callback(self, msg):
+        self.current_angle = msg.z
+    
 
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
+    def execute_callback(self, goal_handle):
+        desired_rotation = goal_handle.request.goal
+        feedback_msg = Rotate.Feedback()
+        last_angle = self.current_angle
+        goal = self.current_angle + desired_rotation
+        cmd_msg = Twist()
+        if desired_rotation > 0: cv = False
+        else: cv = True
+        angle = self.accumulated_angle
+        target_angle = self.rotate_relative(self.accumulated_angle, desired_rotation)
 
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
-        if not goal_handle.accepted:
-            self.get_logger().info('Goal rejected :(')
-            return
+        while True:
+            self.accumulated_angle = self.update_continuous_angle(self.current_angle, last_angle, self.accumulated_angle)
+            last_angle = self.current_angle
 
-        self.get_logger().info('Goal accepted :)')
+            direction = 1 if desired_rotation > 0 else -1
 
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
+            cmd_msg.angular.z = 0.2 * direction
+            self.publisher_.publish(cmd_msg)
+            time.sleep(0.1)
 
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info('Result: {0}'.format(result.sequence))
-        rclpy.shutdown()
+            if (desired_rotation > 0 and self.accumulated_angle >= target_angle) or \
+            (desired_rotation < 0 and self.accumulated_angle <= target_angle):
+                break
 
-    def feedback_callback(self, feedback_msg):
-        feedback = feedback_msg.feedback
-        self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
+        cmd_msg.angular.z = 0.0
+        self.publisher_.publish(cmd_msg)
+            
+        goal_handle.succeed()
+        result = Rotate.Result()
+        result.result = "Done!"
+        self.get_logger().info(f'Result: {result.result}')
+        return result
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    action_client = FibonacciActionClient()
+    rotate_action_server = RotationActionServer()
 
-    action_client.send_goal(10)
+    executor = MultiThreadedExecutor()
+    executor.add_node(rotate_action_server)
+    executor.spin()
 
-    rclpy.spin(action_client)
-
+    rotate_action_server.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
